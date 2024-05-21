0892dc810e4ba316aeaaaba53002549e576da3a8
Elias <44979236+eli-che@users.noreply.github.com>
2024-05-21 12:53:51 +0200

    Route the main application page to home url.
    The base url will now display the application instead of /PoliceActivity/Index

--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=PoliceActivity}/{action=Index}/{id?}");
 
 app.Run();
